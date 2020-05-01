68a13803118df86cb6115ff879466fa23a0714be
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-01 14:16:12 -0600

    try

--- src/actions/reviewActions.js
+++ src/actions/reviewActions.js
@@ -1,5 +1,26 @@
 import actionTypes from '../constants/actionTypes';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
+/*
+export function submitReview(review){
+    const env = runtimeEnv();
+    return dispatch => {
+        return fetch(`${env.REACT_APP_API_URL}/reviews`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(review),
+            mode: 'cors'})
+            .then( (response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .catch( (e) => console.log(e) );
+    }
+}*/
 
 function reviewPosted(review){
     return {
@@ -10,15 +31,15 @@ function reviewPosted(review){
 
 export function submitReview(review){
     const env = runtimeEnv();
-    var formBody = [];
-    for(var property in review)
-    {
-        var encodedKey = encodeURIComponent(property);
-        var encodedValue = encodeURIComponent(review[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-    }
-    formBody = formBody.join("&");
-
+	var formBody = [];
+	for(var property in review)
+	{
+		var encodedKey = encodeURIComponent(property);
+		var encodedValue = encodeURIComponent(review[property]);
+		formBody.push(encodedKey + "=" + encodedValue);
+	}
+	formBody = formBody.join("&");
+	
     return dispatch => {
         return fetch(`${env.REACT_APP_API_URL}/reviews`, {
             method: 'POST',
@@ -27,7 +48,7 @@ export function submitReview(review){
                 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
                 'Authorization': localStorage.getItem('token')
             },
-            body: formBody,
+			body: formBody,
             mode: 'cors'})
             .then( (response) => {
                 if (!response.ok) {
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -7,6 +7,8 @@ import { Carousel } from 'react-bootstrap'
 import { Glyphicon } from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap';
 
+//require a callback function to be sent to MovieList to update the header subtitle
+
 class MovieList extends Component {
     constructor(props) {
         super(props);
@@ -29,6 +31,7 @@ class MovieList extends Component {
     }
 
     render() {
+
         const MovieListCarousel= ({movieList}) => {
             if (!movieList) { // evaluates to true if currentMovie is null
                 return <div>Loading...</div>;
@@ -37,22 +40,22 @@ class MovieList extends Component {
             return (
                 <Carousel onSelect={this.handleSelect}>
                     {movieList.map((movie) =>
-                        <Carousel.Item key={movie._id}>
-                            <div>
-                                <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
-                                    <Image className="image" src={movie.imageUrl} thumbnail />
-                                </LinkContainer>
-                            </div>
-                            <Carousel.Caption>
-                                <h3>{movie.title}</h3>
-                                <Glyphicon glyph={'star'} /> {movie.averageRating} &nbsp;&nbsp; {movie.year_released}
-                            </Carousel.Caption>
-                        </Carousel.Item>)}
-                </Carousel>)
+                    <Carousel.Item key={movie._id}>
+                        <div>
+                            <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
+                                <Image className="image" src={movie.imageUrl} thumbnail />
+                            </LinkContainer>
+                        </div>
+                        <Carousel.Caption>
+                            <h3>{movie.title}</h3>
+                            <Glyphicon glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.yearReleased}
+                        </Carousel.Caption>
+                    </Carousel.Item>)}
+            </Carousel>);
         }
 
         return (
-          <MovieListCarousel movieList={this.props.movies} />
+            <MovieListCarousel movieList={this.props.movies} />
         );
     }
 }
