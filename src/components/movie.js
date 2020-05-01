01a239ad539092e815404ff6bd470667d281c540
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-01 10:35:20 -0600

    shawn

--- src/components/movie.js
+++ src/components/movie.js
@@ -11,52 +11,50 @@ class Movie extends Component {
 
     componentDidMount() {
         const {dispatch} = this.props;
-        if (this.props.selectedMovie == null)
+        if (this.props.selectedMovie == null) {
             dispatch(fetchMovie(this.props.movieId));
+        }
     }
 
     render() {
         const ActorInfo = ({actors}) => {
             return actors.map((actor, i) =>
                 <p key={i}>
-                    <b>{actors.Actor1.ActorName}</b> {actors.Actor1.CharacterName}
-                    <p></p>
-                    <b>{actors.Actor2.ActorName}</b> {actors.Actor2.CharacterName}
-                    <p></p>
-                    <b>{actors.Actor3.ActorName}</b> {actors.Actor3.CharacterName}
+                    <b>{actor.actorName}</b> {actor.characterName}
                 </p>
-            );
-        };
+            )
+        }
 
         const ReviewInfo = ({reviews}) => {
             return reviews.map((review, i) =>
                 <p key={i}>
-                    <b>{review.username}</b> {review.comment}
+                    <b>{review.username}</b> {review.review}
                     <Glyphicon glyph={'star'} /> {review.rating}
                 </p>
-            );
+            )
         }
 
         const DetailInfo = ({currentMovie}) => {
-            if (!currentMovie) { // evaluates to true if currentMovie is null
+            if (!currentMovie) { //if not could still be fetching the movie
                 return <div>Loading...</div>;
             }
             return (
-                <Panel>
-                    <Panel.Heading>Movie Detail</Panel.Heading>
-                    <Panel.Body><Image className="image" src={currentMovie.ImageUrl} thumbnail /></Panel.Body>
-                    <ListGroup>
-                        <ListGroupItem>{currentMovie.title}</ListGroupItem>
-                        <ListGroupItem><ActorInfo Actors={currentMovie.actors} /></ListGroupItem>
-                        <ListGroupItem><h4><Glyphicon glyph={'star'} /> {currentMovie.averageRating} </h4></ListGroupItem>
-                    </ListGroup>
-                    <Panel.Body><ReviewInfo Reviews={currentMovie.reviews} /></Panel.Body>
-                </Panel>
+              <Panel>
+                  <Panel.Heading>Movie Detail</Panel.Heading>
+                  <Panel.Body><Image className="image" src={currentMovie.imageUrl} thumbnail /></Panel.Body>
+                  <ListGroup>
+                      <ListGroupItem>{currentMovie.title}</ListGroupItem>
+                      <ListGroupItem><ActorInfo actors={currentMovie.actors} /></ListGroupItem>
+                      <ListGroupItem><h4><Glyphicon glyph={'star'}/> {currentMovie.avgRating} </h4></ListGroupItem>
+                  </ListGroup>
+                  <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
+              </Panel>
             );
-        };
+        }
+
         return (
             <DetailInfo currentMovie={this.props.selectedMovie} />
-        );
+        )
     }
 }
 
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -40,12 +40,12 @@ class MovieList extends Component {
                         <Carousel.Item key={movie._id}>
                             <div>
                                 <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
-                                    <Image className="image" src={movie.imageURL} thumbnail />
+                                    <Image className="image" src={movie.imageUrl} thumbnail />
                                 </LinkContainer>
                             </div>
                             <Carousel.Caption>
                                 <h3>{movie.title}</h3>
-                                <Glyphicon glyph={'star'} /> {movie.averageRating} &nbsp;&nbsp; {movie.year}
+                                <Glyphicon glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
                             </Carousel.Caption>
                         </Carousel.Item>)}
                 </Carousel>)
