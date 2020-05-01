a21a1e4b6d2664182cd23b639ddb397db28a8dcc
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-01 11:28:45 -0600

    var name

--- src/components/movie.js
+++ src/components/movie.js
@@ -28,7 +28,7 @@ class Movie extends Component {
         const ReviewInfo = ({reviews}) => {
             return reviews.map((review, i) =>
                 <p key={i}>
-                    <b>{review.username}</b> {review.review}
+                    <b>{review.username}</b> {review.comment}
                     <Glyphicon glyph={'star'} /> {review.rating}
                 </p>
             )
@@ -45,7 +45,7 @@ class Movie extends Component {
                   <ListGroup>
                       <ListGroupItem>{currentMovie.title}</ListGroupItem>
                       <ListGroupItem><ActorInfo actors={currentMovie.actors} /></ListGroupItem>
-                      <ListGroupItem><h4><Glyphicon glyph={'star'}/> {currentMovie.avgRating} </h4></ListGroupItem>
+                      <ListGroupItem><h4><Glyphicon glyph={'star'}/> {currentMovie.averageRating} </h4></ListGroupItem>
                   </ListGroup>
                   <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
               </Panel>
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -45,7 +45,7 @@ class MovieList extends Component {
                             </div>
                             <Carousel.Caption>
                                 <h3>{movie.title}</h3>
-                                <Glyphicon glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+                                <Glyphicon glyph={'star'} /> {movie.averageRating} &nbsp;&nbsp; {movie.year_released}
                             </Carousel.Caption>
                         </Carousel.Item>)}
                 </Carousel>)
